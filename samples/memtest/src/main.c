ed956b0c8faa8d60fa0c2491fcdb8cf9ad4e1e60
itsoctotv <octoyt111@gmail.com>
2023-11-15 13:23:07 +0100

    sync

--- samples/memtest/src/main.c
+++ samples/memtest/src/main.c
@@ -8,72 +8,81 @@
 #include <stdint.h>
 #include <zephyr/kernel.h>
 
-void memWrite(uint32_t addr, int32_t data);
+void memWrite(uint32_t addr, char data);
 void memRead(uint32_t addr);
 
-#define LIMIT			1024
+//#define LIMIT			1024
 #define ADDRESS			0x60000000
 /*
-
-TODO:
-fill the PSRAM 512KB (struct bit array)
-
-Random locations with pointer and counter that counts lineary and fills the specific memory address
 
 maybe:
 - https://barrgroup.com/embedded-systems/how-to/memory-test-suite-c
 
 */
 
-// !!! https://stackoverflow.com/questions/5610298/why-does-int-pointer-increment-by-4-rather-than-1 !!!
+// https://stackoverflow.com/questions/5610298/why-does-int-pointer-increment-by-4-rather-than-1
 
 
 
 int main(){
 
 
-	uint32_t addr = ADDRESS;
-	for(int i = 0; i < LIMIT; i++){
-
-		memRead(addr);
-		addr += sizeof(uint32_t);
-		
-	}
-
-	int32_t count = 1;
-	addr = ADDRESS;
-	printf("WRITING MEM\n");
-	k_msleep(1000);
-	for(int i = 0; i < LIMIT; i++){
+	uint32_t address = ADDRESS;
+	int val = 1;
+	/*
+	printf("%x, %x\n",address, val);
+	address++;
+	printf("%x\n",address);
 	
-		memWrite(addr, count);
-		/*if(count > 0xff){
-			count = 0;
-		}*/
-		count++;
-		addr += sizeof(uint32_t);
-	}
-	printf("DONE\n");
-	k_msleep(1000);
-	addr = ADDRESS;
-	for(int i = 0; i < LIMIT; i++){
-
-		memRead(addr);
-		addr += sizeof(uint32_t);
+*/ 
+	//printf("start writing 0s\n");
+	/*for(int i = 0; i < 3000; i++){
 		
+		memWrite(address, 0);
+		k_msleep(10);
+		val++;
+		address++;
+	}*/
+	printf("Done\nStart writing mem\n");
+	address = ADDRESS;
+	
+	for(int i = 0; i < 3000; i++){
+		
+		memWrite(address, val);
+		
+
+		val++;
+		address++;
+		//k_usleep(100);
 	}
+	printf("Done\nReading mem\n");
+	address = ADDRESS;
+	for(int i = 0; i < 3000; i++){
+
+		memRead(address);
+		address++;
+		//k_usleep(100);
+	}
+	//printf("%x, %x\n",address, val);
+	//printf("%x\n",address);
+	/*
+	memRead(address);
+	memWrite(address, val);
+	memRead(address);
+	*/
 
 	printf("memtest complete");
 	return 0;
 }
 
-void memWrite(uint32_t addr, int32_t data){
-	volatile uint32_t *ptr = (uint32_t *)addr;
+void memWrite(uint32_t addr, char data){
+	volatile uint32_t* ptr = (uint32_t *)addr;
 	*ptr = data;
 }
 
 void memRead(uint32_t addr){
-	volatile uint32_t *readaddr = (uint32_t *)addr;
-	printf("%p: %08x \n",readaddr, *readaddr);
+	volatile uint32_t* readaddr = (uint32_t *)addr;
+	char read_val = *readaddr;
+	printf("%p: %08d \n",readaddr, read_val);
 }
 
